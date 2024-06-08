8afed6ce04943814135aa0016e7399a1a0a53d84
jilikuo <joaovitorbariri@hotmail.com>
2024-06-07 23:09:29 -0300

    modificações relacionadas a tela de game over e adição de limites no cenário

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,7 +11,7 @@ public class PlayerMovement : MonoBehaviour
         
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
