68556312ef8a5925c060c1ef6a00cd2ddd56cc80
jilikuo <54220633+jilikuo@users.noreply.github.com>
2024-06-11 14:35:47 -0300

    adicionado acréscimo gradual de dificuldade
    baseado no total de objetos spawnados + pontuacao do jogador

--- Assets/Scripts/ObstacleManager.cs
+++ Assets/Scripts/ObstacleManager.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class ObstacleManager : MonoBehaviour
 {
     public float cooldown;
-    public GameObject obstacle;
     private float initial_cooldown;
+    public float current_cooldown;
+    private int spawned_obstacles;
+
+    public GameObject obstacle;
+    public GameObject score_manager;
 
     private void Start()
     {
         initial_cooldown = cooldown;
+        current_cooldown = initial_cooldown;
+        spawned_obstacles = 0;
     }
 
     private void FixedUpdate()
     {
         SpawnObstacle();
+        cooldown_manager();
     }
 
     private void SpawnObstacle()
@@ -25,9 +32,16 @@ public class ObstacleManager : MonoBehaviour
             cooldown -= Time.deltaTime;
         }
         else
-        {
+        { 
             Instantiate(obstacle, new Vector3(5, Random.Range(-2.5f, 2.5f), 0), Quaternion.identity);
-            cooldown = initial_cooldown;
+            cooldown = current_cooldown;
+            spawned_obstacles += 1;
         }
     }
+
+    private void cooldown_manager()
+    {
+        current_cooldown = (initial_cooldown - score_manager.GetComponent<ScoreManager>().score/500f - spawned_obstacles/80f);
+    }
+
 }
